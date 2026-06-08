54accc8fc25fef92d8de3ea331af3689458f41b7
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-06-08 23:52:08 +0700

    Add inlines for ProductSKU and Promotion in admin

--- bakery/admin_site.py
+++ bakery/admin_site.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductSKU, Promotion
+
+class ProductSKUInline(admin.TabularInline):
+    model = ProductSKU
+    extra = 1
+
+class PromotionInline(admin.TabularInline):
+    model = Promotion
+    extra = 0
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'is_available', 'created_at']
     list_editable = ['is_available']
+    inlines = [ProductSKUInline, PromotionInline]
