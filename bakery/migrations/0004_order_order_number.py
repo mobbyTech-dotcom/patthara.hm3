d3d4d114279bcdcc13e3eb280d69a69ee9c4bd36
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-06-08 22:47:49 +0700

    Add unique order_number field to Order model
    This migration adds a unique order number field to the Order model and populates it with randomly generated values.

--- bakery/migrations/0004_order_order_number.py
+++ bakery/migrations/0004_order_order_number.py
@@ -1,5 +1,21 @@
 from django.db import migrations, models
 import bakery.models
+import random
+import string
+
+
+def set_unique_order_numbers(apps, schema_editor):
+    Order = apps.get_model('bakery', 'Order')
+    used = set()
+    for order in Order.objects.all():
+        while True:
+            chars = string.ascii_uppercase + string.digits
+            num = 'PT-' + ''.join(random.choices(chars, k=8))
+            if num not in used:
+                used.add(num)
+                order.order_number = num
+                order.save(update_fields=['order_number'])
+                break
 
 
 class Migration(migrations.Migration):
@@ -9,7 +25,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # เพิ่ม field ก่อนโดยไม่ unique และมี default ชั่วคราว
         migrations.AddField(
+            model_name='order',
+            name='order_number',
+            field=models.CharField(max_length=20, default='PT-TEMP', verbose_name='เลขออเดอร์'),
+        ),
+        # กำหนดค่า unique ให้แต่ละ row
+        migrations.RunPython(set_unique_order_numbers, migrations.RunPython.noop),
+        # แล้วค่อย set unique=True
+        migrations.AlterField(
             model_name='order',
             name='order_number',
             field=models.CharField(
