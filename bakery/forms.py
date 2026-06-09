85e9bc53883e36c4408bf120b5df8015386abf43
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-06-09 23:28:40 +0700

    Update forms.py

--- bakery/forms.py
+++ bakery/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Product
 
-
 class ProductForm(forms.ModelForm):
     class Meta:
-        model  = Product
-        fields = ['name', 'description', 'image']   # ลบ 'price' ออก เพราะราคาอยู่ใน SKU แล้ว
+        model = Product
+        # เอา 'price' ออกจาก fields เพราะเราย้ายไปตั้งราคาใน SKU แล้วครับ
+        fields = ['name', 'description', 'image']
         widgets = {
             'name': forms.TextInput(attrs={
                 'class': 'w-full border rounded-xl px-3.5 py-2 text-sm focus:ring-2 focus:ring-blue-400',
