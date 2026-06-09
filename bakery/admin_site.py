b73ecac9434a89ecb42f6cbb2a21bf0abbba7428
mobbyTech-dotcom <mrlxnlypx@gmail.com>
2026-06-09 09:42:47 +0700

    Add files via upload

--- bakery/admin_site.py
+++ bakery/admin_site.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from .models import Product, ProductSKU, Promotion
 
+
 class ProductSKUInline(admin.TabularInline):
-    model = ProductSKU
-    extra = 1
+    model  = ProductSKU
+    extra  = 1
+    fields = ['name', 'price']
+
 
 class PromotionInline(admin.TabularInline):
-    model = Promotion
-    extra = 0
+    model  = Promotion
+    extra  = 0
+    fields = ['min_quantity', 'special_price']
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'is_available', 'created_at']
+    list_display  = ['name', 'is_available', 'created_at']   # ลบ 'price' ออก เพราะย้ายไป SKU แล้ว
     list_editable = ['is_available']
-    inlines = [ProductSKUInline, PromotionInline]
+    inlines       = [ProductSKUInline, PromotionInline]
